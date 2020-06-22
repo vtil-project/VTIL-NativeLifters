b3c5319cbede8d6d3ba58564909a0ea46f85c21e
DefCon42 <defcon4200@outlook.com>
2020-06-22 11:08:41 -0400

    Update amd64.hpp and tests to reflect VTIL-Core changes

--- NativeLifters-Core/amd64/amd64.hpp
+++ NativeLifters-Core/amd64/amd64.hpp
@@ -8,36 +8,18 @@
 // This file defines any global arch-specific information for the AMD64 target
 // architecture.
 //
-namespace vtil
+namespace vtil::lifter::amd64
 {
-	// Allows creation of register_desc's from the x86_reg enumeration
-	//
-	template<>
-	struct register_cast< x86_reg >
-	{
-		register_desc operator()( x86_reg value )
-		{
-			auto [base, offset, size] = amd64::resolve_mapping( value );
-			return register_desc( register_physical, base, size * 8, offset * 8 );
-		}
-	};
+	using instruction_info = vtil::amd64::instruction;
+	using operand_info = cs_x86_op;
 
-	namespace lifter
-	{
-		using instruction_info = vtil::amd64::instruction;
-		using operand_info = cs_x86_op;
+	inline std::map<x86_insn, void( * )( basic_block* block, const instruction_info& insn )> operand_mappings;
 
-		namespace amd64
-		{
-			inline std::map<x86_insn, void( * )( basic_block* block, const instruction_info& insn )> operand_mappings;
-			
-			void initialize_mappings( );
+	void initialize_mappings( );
 
-			register_desc get_disp_from_operand( basic_block* block, const operand_info& operand );
-			operand load_operand( basic_block* block, const instruction_info& insn, size_t idx );
-			void store_operand( basic_block* block, const instruction_info& insn, size_t idx, const operand& source );
+	register_desc get_disp_from_operand( basic_block* block, const operand_info& operand );
+	operand load_operand( basic_block* block, const instruction_info& insn, size_t idx );
+	void store_operand( basic_block* block, const instruction_info& insn, size_t idx, const operand& source );
 
-			void process( instruction_info& insn, basic_block* block );
-		}
-	}
+	void process( instruction_info& insn, basic_block* block );
 }
--- NativeLifters-Tests/main.cpp
+++ NativeLifters-Tests/main.cpp
@@ -9,11 +9,11 @@ int main( int argc, char** argv )
 	basic_block* blk = basic_block::begin( 0 );
 	blk->owner->routine_convention = preserve_all_convention;
 
-	uint8_t code [ ] { 0x48, 0x31, 0xC0, 0x48, 0x83, 0xE8, 0x01, 0x48, 0xC1, 0xE0, 0x0A, 0x48, 0xC1, 0xE8, 0x0A };
+	uint8_t code [ ] { 0x48, 0x8D, 0x00 };
 
 	lifter::amd64::initialize_mappings( );
 
-	auto insns = capstone::disasm( code, 0, sizeof( code ) );
+	auto insns = amd64::disasm( code, 0, sizeof( code ) );
 	for ( auto& ins : insns )
 	{
 		printf( "[LIFT] %s\n", ins.to_string().c_str() );
