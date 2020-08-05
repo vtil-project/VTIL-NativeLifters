5ddf196b104044d5221ba61625d643b75c3255f8
Altair <maciejkamat@gmail.com>
2020-08-05 18:14:38 +0200

    Tests for SHLD/SHRD

--- NativeLifters-Tests/main.cpp
+++ NativeLifters-Tests/main.cpp
@@ -137,6 +137,57 @@ pop rbx)");
 		leave
 )");*/
 
+	TEST("shld ax, bx, 0");
+	TEST("shld ax, bx, 1");
+	TEST("shld ax, bx, 15");
+	TEST(R"(
+		and cl, 15
+		shld ax, bx, cl
+		)");
+	TEST("shld eax, ebx, 0");
+	TEST("shld eax, ebx, 1");
+	TEST("shld eax, ebx, 31");
+	TEST(R"(
+		and cl, 31
+		shld eax, ebx, cl
+		)");
+	TEST("shld rax, rbx, 0");
+	TEST("shld rax, rbx, 1");
+	TEST("shld rax, rbx, 16");
+	TEST("shld rax, rbx, 31");
+	TEST("shld rax, rbx, 32");
+	TEST("shld rax, rbx, 60");
+	TEST("shld rax, rbx, 63");
+	TEST(R"(
+		and cl, 63
+		shld rax, rbx, cl
+		)");
+
+	TEST("shrd ax, bx, 0");
+	TEST("shrd ax, bx, 1");
+	TEST("shrd ax, bx, 3");
+	TEST(R"(
+		and cl, 15
+		shrd ax, bx, cl
+		)");
+	TEST("shrd eax, ebx, 0");
+	TEST("shrd eax, ebx, 1");
+	TEST("shrd eax, ebx, 31");
+	TEST(R"(
+		and cl, 31
+		shrd eax, ebx, cl
+		)");
+	TEST("shrd rax, rbx, 0");
+	TEST("shrd rax, rbx, 1");
+	TEST("shrd rax, rbx, 16");
+	TEST("shrd rax, rbx, 31");
+	TEST("shrd rax, rbx, 32");
+	TEST("shrd rax, rbx, 63");
+	TEST(R"(
+		and cl, 63
+		shrd rax, rbx, cl
+		)");
+
 	size_t passed = 0;
 	for (size_t i = 0; i < tests.size(); i++)
 	{
